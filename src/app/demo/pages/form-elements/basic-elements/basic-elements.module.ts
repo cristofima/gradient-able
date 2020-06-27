c51970637e5e5e421026a936b851b7c8042fcf9c
Cristopher Coronado Moreira <cristofima@hotmail.com>
2020-06-27 16:49:21 -0500

    Forms with PrimeNG

--- src/app/demo/pages/form-elements/basic-elements/basic-elements.module.ts
+++ src/app/demo/pages/form-elements/basic-elements/basic-elements.module.ts
@@ -5,12 +5,14 @@ import { BasicElementsRoutingModule } from './basic-elements-routing.module';
 import { BasicElementsComponent } from './basic-elements.component';
 import {SharedModule} from '../../../../theme/shared/shared.module';
 import {NgbDropdownModule} from '@ng-bootstrap/ng-bootstrap';
+import { AppPrimeNgModule } from '../../../../app.primeng.module';
 
 @NgModule({
   imports: [
     CommonModule,
     BasicElementsRoutingModule,
     SharedModule,
+    AppPrimeNgModule,
     NgbDropdownModule
   ],
   declarations: [BasicElementsComponent]
--- src/app/demo/pages/form-elements/form-elements.module.ts
+++ src/app/demo/pages/form-elements/form-elements.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { FormElementsRoutingModule } from './form-elements-routing.module';
 import {SharedModule} from '../../../theme/shared/shared.module';
+import { AppPrimeNgModule } from 'src/app/app.primeng.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormElementsRoutingModule,
-    SharedModule
+    SharedModule,
+    AppPrimeNgModule
   ],
   declarations: []
 })
